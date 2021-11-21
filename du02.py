05f4eda153e6e91f78e07a5038c9685305f9a5a0
Václav Thám <venca275tham@gmail.com>
2021-11-21 17:34:40 +0100

    funguje pro sedmidenní prumery a max a min prutok

--- du02.py
+++ du02.py
@@ -1,39 +1,52 @@
 import csv
 from os import linesep
 with open("vstup.csv", encoding="utf-8") as csvinfile,\
-	open("vystup_7dni.csv", "w", encoding="utf-8") as csvoutfile:
+	 open("vystup_7dni.csv", "w", newline="", encoding="utf-8") as csvoutfile:
 	# Vytvoříme proměnnou `reader` držící objekt pro čtení z `csvinfile`
 	reader = csv.reader(csvinfile, delimiter = ",")
-	# Vytvoříme proměnnou `writer` držící objekt pro zápis do `csvoutfile`
-	writer = csv.writer(csvoutfile)
-	
+	# Vytvoříme proměnnou `writer`, jak pro tydenni prumery tak pro rocni držící objekt pro zápis do `csvoutfile`
+	writertyden = csv.writer(csvoutfile)
+	writerrok = csv.writer(csvoutfile)
+	# Vytvorime si proměnné sedmiprum(prumer za 7 dni) a promennou cisloradky
 	sedmiprum = 0
-	cisloradky=-1
+	cisloradky= 0
+	zbytek = 0	
+	# Cyklus, který projede všechny řádky v souboru
 	for row in reader:		# Čteme jednotlivé řádky z `reader`u, v `row` máme seznam buněk daného řádku
-		cisloradky= cisloradky + 1
-		sedmdni= cisloradky % 7
+		# Zjišťujeme zda je cisloradky delitelné 7
+		if cisloradky % 7 == 0:
+			prvnidensedmice = row
+		# Přiřadí do sedmiprum, denni prumer z aktualni radky
 		try:
-				sedmiprum = sedmiprum + float(row[5])		# Kratší forma zápisu
-				#total = total + int(row[4])	# Delší forma zápisu, dělá to samé, co kratší
+				sedmiprum = sedmiprum + float(row[5])
+				zbytek = zbytek + 1		
 		except ValueError:
-			pass	
-		if sedmdni == 0:
+			print("Na řádku je chybná hodnota")
+
+		if cisloradky % 7 == 6:
 			sedmiprum=sedmiprum/7
-			formatted_string="{:.4f}".format(sedmiprum)
-			row[5]=float(formatted_string)
-			print(row[0:6])
+			sedmiprum ="{:.4f}".format(sedmiprum)
+			prvnidensedmice[5]= sedmiprum
+			writertyden.writerow(prvnidensedmice)
+			zbytek= zbytek - 1
+			sedmiprum= float(sedmiprum)
 			sedmiprum=0
-with open("vstup.csv", encoding="utf-8") as csvinfile,\
-	open("vystup_rok.csv", "w", encoding="utf-8") as csvoutfile:
-	# Vytvoříme proměnnou `reader` držící objekt pro čtení z `csvinfile`
-	reader = csv.reader(csvinfile, delimiter = ",")
-	# Vytvoříme proměnnou `writer` držící objekt pro zápis do `csvoutfile`
-	writer = csv.writer(csvoutfile)
+		cisloradky= cisloradky + 1
+	if cisloradky % 7 != 6:
+		sedmiprum = sedmiprum / zbytek
+		sedmiprum = "{:.4f}".format(sedmiprum)
+		prvnidensedmice[5] = sedmiprum
+		writertyden.writerow(prvnidensedmice)
+
+# Maximální průtok
 with open("vstup.csv", encoding="utf-8") as csvinfile:
 	reader = csv.reader(csvinfile, delimiter = ",")
 	maxprutok=max(reader, key=lambda row: float(row[5]))
+	print("Toto je den s maximálním průtokem")
 	print(maxprutok)
+# Minimální průtok
 with open("vstup.csv", encoding="utf-8") as csvinfile:
 	reader = csv.reader(csvinfile, delimiter = ",")
 	minprutok=min(reader, key=lambda row: float(row[5]))
+	print("Toto je den s minimálním průtokem")
 	print(minprutok)
