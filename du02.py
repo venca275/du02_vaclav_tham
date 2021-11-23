63303f2d47e2c4838b4443692067e32d9333f0f0
Václav Thám <venca275tham@gmail.com>
2021-11-23 16:18:07 +0100

    v rámci možností funguje, ale chce to doladit

--- du02.py
+++ du02.py
@@ -1,9 +1,5 @@
 import csv
 from os import linesep
-with open("vstup.csv", encoding="utf-8") as csvinfile:
-	reader = csv.reader(csvinfile, delimiter = ",")
-	celkpocetradek = len(list(reader))
-	celkpocetradek = int(celkpocetradek)
 with open("vstup.csv", encoding="utf-8") as csvinfile,\
 	 open("vystup_7dni.csv", "w", newline="", encoding="utf-8") as csvoutfile, \
 	 open("vystup_rok.csv", "w", newline="", encoding="utf-8") as csvoutfile1:
@@ -18,19 +14,29 @@ with open("vstup.csv", encoding="utf-8") as csvinfile,\
 	zbytek = 0
 	prumrok = 0
 	pocetdnuvroce = 0
+	max=0
+	min=60000
 	# Cyklus, který projede všechny řádky v souboru
 	for row in reader:		# Čteme jednotlivé řádky z `reader`u, v `row` máme seznam buněk daného řádku
-		# Zjišťujeme zda je cisloradky delitelné 7
+		# Zjišťujeme zda je cisloradky delitelné 7 a skončí dělení nulou, poté přiřadíme aktualní řádku do proměnné
 		if cisloradky % 7 == 0:
 			prvnidensedmice = row
 		
-		# Přiřadí do sedmiprum, denni prumer z aktualni radky
+		# Přiřadí do sedmidenního průměru, denní prumer z aktualni řádky
+		# také připočítá zbytek + 1, kvůli tomu abychom zajistili správný výpočet pokud by počet řádek nebyl dělitelný 7
 		try:
 				sedmiprum = sedmiprum + float(row[5])
 				zbytek = zbytek + 1		
 		except ValueError:
 			print("Na řádku je chybná hodnota")
-		
+		# Maximální denní průtok
+		if float(row[5]) > float(max):
+			maxprutok=row
+			max = row[5]
+		# Minimální denní průtok
+		if float(row[5]) < float(min):
+			minimalprutok=row
+			min = row[5]
 		# Přiřadí do prumrok, denni prumer z aktualni radky
 		try:
 				prumrok = prumrok + float(row[5])		
@@ -42,7 +48,7 @@ with open("vstup.csv", encoding="utf-8") as csvinfile,\
 			sedmiprum ="{:.4f}".format(sedmiprum)
 			prvnidensedmice[5]= sedmiprum
 			writertyden.writerow(prvnidensedmice)
-			zbytek= zbytek - 1
+			zbytek = 0
 			sedmiprum= float(sedmiprum)
 			sedmiprum=0
 		cisloradky= cisloradky + 1
@@ -52,7 +58,7 @@ with open("vstup.csv", encoding="utf-8") as csvinfile,\
 			prvnidenroku = row
 			pocitanyrok = int(prvnidenroku[2])
 		aktualnirok = int(row[2])
-		if pocitanyrok != aktualnirok or cisloradky == celkpocetradek:
+		if pocitanyrok != aktualnirok:
 			prumrok = prumrok / pocetdnuvroce
 			prumrok ="{:.4f}".format(prumrok)
 			prvnidenroku[5] = prumrok
@@ -63,27 +69,22 @@ with open("vstup.csv", encoding="utf-8") as csvinfile,\
 			prvnidenroku=row
 			pocitanyrok = int(prvnidenroku[2])
 		
-		#if cisloradky == celkpocetradek:
-		#	prumrok = prumrok / pocetdnuvroce
-		#	prumrok ="{:.4f}".format(prumrok)
-		#	prvnidenroku[5] = prumrok
-		#	writerrok.writerow(prvnidenroku)
 		pocetdnuvroce = pocetdnuvroce + 1
+	# Výpočet prumeru a vypsani, pokud celkový počet řádek není dělitelný 7
 	if cisloradky % 7 != 6:
 		sedmiprum = sedmiprum / zbytek
 		sedmiprum = "{:.4f}".format(sedmiprum)
 		prvnidensedmice[5] = sedmiprum
 		writertyden.writerow(prvnidensedmice)
+	# Zapsání posledního roku v tabulce po skončení cyklu
+	prumrok = prumrok / pocetdnuvroce
+	prumrok ="{:.4f}".format(prumrok)
+	prvnidenroku[5] = prumrok
+	writerrok.writerow(prvnidenroku)
 
 # Maximální průtok
-with open("vstup.csv", encoding="utf-8") as csvinfile:
-	reader = csv.reader(csvinfile, delimiter = ",")
-	maxprutok=max(reader, key=lambda row: float(row[5]))
 	print("Toto je den s maximálním průtokem")
 	print(maxprutok)
 # Minimální průtok
-with open("vstup.csv", encoding="utf-8") as csvinfile:
-	reader = csv.reader(csvinfile, delimiter = ",")
-	minprutok=min(reader, key=lambda row: float(row[5]))
 	print("Toto je den s minimálním průtokem")
-	print(minprutok)
+	print(minimalprutok)
