b7a7aea6b6ddaa61bea48d6c11cfd8b4ff310c68
Václav Thám <venca275tham@gmail.com>
2021-11-27 15:02:51 +0100

    Hotovo

--- du02.py
+++ du02.py
@@ -1,5 +1,6 @@
 import csv
 from os import linesep
+from datetime import datetime, timedelta
 try:
 	with open("vstup.csv", encoding="utf-8") as csvinfile,\
 		open("vystup_7dni.csv", "w", newline="", encoding="utf-8") as csvoutfile, \
@@ -19,71 +20,24 @@ try:
 		mindenniprutok=float('inf')
 		# Cyklus, který projede všechny řádky v souboru
 		for row in reader:
-			
-			# Detekce chyb záporný nebo nulový průtok
 			try:
-				if float(row[5]) <= 0:
-					print(f"Průtok dne {row[4]}.{[3]}.{[2]} je buďto záporný nebo nulový")
-			except ValueError:
-				pass
-			except IndexError:
-				pass
-			# Detekce chyb - chybí den
-			mesicsoucasn=int(row[3])
-			densoucasny=int(row[4])
-			if reader.line_num == 1:
-				mesicpredchozi=int(row[3])
-				denpredchozi=int(row[4])
-			if reader.line_num > 1:
-				if densoucasny != denpredchozi + 1 and mesicsoucasn==mesicpredchozi:
-					chybnyden=denpredchozi + 1
-					print(f"V seznamu chybí den {chybnyden}.{row[3]}.{row[2]}")
-				if denpredchozi != 1 and densoucasny==2:
-					chybnyden= densoucasny -1
-					print(f"V seznamu chybí den {chybnyden}.{row[3]}.{row[2]}")
-			mesicpredchozi=int(row[3])
-			denpredchozi=int(row[4])
-			
-			# Přiřazení prvního dne v týdnu
-			if cisloradky % 7 == 0:
-				prvnidensedmice = row
-			
-			# Přičte do sedmidenního průměru, denní prumer z aktualni řádky
-			# také počítá zbytek
-			try:
-					sedmiprum = sedmiprum + float(row[5])
-					zbytek = zbytek + 1		
-			except ValueError:
-				pass
-			except IndexError:
-				pass
-			
-			# Maximální denní průtok
-			try:
-				if float(row[5]) > float(maxdenniprutok):
-					maxhodnoty=row
-					maxdenniprutok = row[5]
-			except ValueError:
-				pass
-			except IndexError:
-				pass
-			# Minimální denní průtok
-			try:
-				if float(row[5]) < float(mindenniprutok):
-					minhodnoty=row
-					mindenniprutok = row[5]
-			except ValueError:
-				pass
-			except IndexError:
-				pass
-			
-			# Přičte do ročního průměru, denni prumer z aktualni radky
-			try:
-					prumrok = prumrok + float(row[5])		
+				denniprum=float(row[5])
 			except ValueError:
 				print(f"Na řádku {reader.line_num} je chybná hodnota")
 			except IndexError:
 				print(f"Na řádku {reader.line_num} je nesprávně dlouhé pole")
+
+			# Detekce chyb záporný nebo nulový průtok
+			if (denniprum) <= 0:
+				print(f"Průtok dne {row[4]}.{row[3]}.{row[2]} je buďto záporný nebo nulový")
+						
+			# Přiřazení prvního dne v týdnu
+			if cisloradky % 7 == 0:
+				prvnidensedmice = row
+			# Přičte do sedmidenního průměru, denní prumer z aktualni řádky
+			# také počítá zbytek			
+			sedmiprum = sedmiprum + (denniprum)
+			zbytek = zbytek + 1			
 			#  Zjístí, že jsme na posledním dni 7 tak vypočte průměr za týden přiřadí do první řádky sedmice, a následně zapíše
 			if cisloradky % 7 == 6:
 				sedmiprum= sedmiprum/7
@@ -94,6 +48,8 @@ try:
 				sedmiprum=0
 			cisloradky= cisloradky + 1
 			
+			# Přičte do ročního průměru, denni prumer z aktualni radky
+			prumrok = prumrok + (denniprum)
 			# Podmínky pro přirazení prvního dne roku do proměnné
 			# Podmínka pro první řádku
 			if reader.line_num == 1:
@@ -110,8 +66,18 @@ try:
 				pocetdnuvroce=0
 				prvnidenroku=row
 				pocitanyrok = int(prvnidenroku[2])
+
+			# Maximální denní průtok
+			if (denniprum) > float(maxdenniprutok):
+				maxhodnoty=row
+				maxdenniprutok = denniprum
+			# Minimální denní průtok
+			if  (denniprum) < float(mindenniprutok):
+				minhodnoty=row
+				mindenniprutok = denniprum
 			
 			pocetdnuvroce = pocetdnuvroce + 1
+			# Konec cyklu
 		
 		# Výpočet prumeru a vypsani, pokud celkový počet řádek není dělitelný 7(mimo cyklus jelikož se udělá pouze jednou na konci)
 		if cisloradky % 7 != 0:
@@ -124,6 +90,7 @@ try:
 		prumrok ="{:.4f}".format(prumrok)
 		prvnidenroku[5] = prumrok
 		writerrok.writerow(prvnidenroku)
+
 
 	# Maximální průtok
 		print(f"Dne {maxhodnoty[4]}.{maxhodnoty[3]}.{maxhodnoty[2]} byl naměřen nejvyšší denní průtok {maxdenniprutok}")
